18587b4b389b40ac021ef2bde37716446e8b78ed
Subhrajitsarkar <subhrajitsarkar28@gmail.com>
2024-10-30 23:56:04 +0530

    updated the code a little bit
    updated the code a little bit

--- 12promiseAll/12bscript.js
+++ 12promiseAll/12bscript.js
@@ -1,21 +1,21 @@
 const posts = [{ title: 'POST1' }];
-let lastActivityTime = null; // This will store the user's last activity time
+let lastActivityTime = null;
 
 function createPost(post) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            posts.push(post); // Adds the new post to the array
-            resolve();        // Resolves the promise once the post is added
-        }, 2000); // Simulate 2 seconds delay to create a post
+            posts.push(post);
+            resolve();
+        }, 2000);
     });
 }
 
 function updateLastUserActivityTime() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            lastActivityTime = new Date(); // Set the current time as the last activity time
-            resolve(lastActivityTime);     // Resolve with the updated activity time
-        }, 1000); // Simulate 1 second delay for updating the activity time
+            lastActivityTime = new Date();
+            resolve(lastActivityTime);
+        }, 1000);
     });
 }
 
@@ -23,32 +23,22 @@ function deletePost() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (posts.length > 0) {
-                const poppedElement = posts.pop(); // Remove the last post from the array
-                resolve(poppedElement);            // Resolve the promise with the deleted post
+                const poppedElement = posts.pop();
+                resolve(poppedElement);
             } else {
-                reject("ERROR: No posts available to delete"); // If no posts to delete, reject the promise
+                reject("ERROR: No posts available to delete");
             }
-        }, 1000); // Simulate 1 second delay for deleting a post
+        }, 1000);
     });
 }
 
-// Creating a new post and updating the activity time
-Promise.all([
-    createPost({ title: 'POST5', body: 'This is POST5' }),  // Creating a new post
-    updateLastUserActivityTime()                            // Updating last activity time
-])
+Promise.all([createPost({ title: 'POST5', body: 'This is POST5' }), updateLastUserActivityTime()])
     .then(([createPostResult, lastActivityTimeResult]) => {
-        // Log all posts and the updated lastActivityTime
         console.log('Posts:', posts);
         console.log('Last Activity Time:', lastActivityTimeResult);
-        // lastActivityTimeResult holds the time from when the user's activity was updated (after 1 second). lastActivityTime = lastActivityTimeResult
-        // However, lastActivityTimeResult is logged after 2 seconds, when the slower createPost promise also resolves.
-
-        // Now delete the last post
         return deletePost();
     })
     .then((deletedPost) => {
-        // Log the remaining posts after deleting the last one
         console.log('Deleted Post:', deletedPost.title);
         console.log('Remaining Posts:', posts);
     })
--- 13asyncAwait/13script.js
+++ 13asyncAwait/13script.js
@@ -1,21 +1,21 @@
 const posts = [{ title: 'POST1' }];
-let lastActivityTime = null; // This will store the user's last activity time
+let lastActivityTime = null;
 
 function createPost(post) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            posts.push(post); // Adds the new post to the array
-            resolve();        // Resolves the promise once the post is added
-        }, 2000); // Simulate 2 seconds delay to create a post
+            posts.push(post);
+            resolve();
+        }, 2000);
     });
 }
 
 function updateLastUserActivityTime() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            lastActivityTime = new Date(); // Set the current time as the last activity time
-            resolve(lastActivityTime);     // Resolve with the updated activity time
-        }, 1000); // Simulate 1 second delay for updating the activity time
+            lastActivityTime = new Date();
+            resolve(lastActivityTime);
+        }, 1000);
     });
 }
 
@@ -23,38 +23,29 @@ function deletePost() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (posts.length > 0) {
-                const poppedElement = posts.pop(); // Remove the last post from the array
-                resolve(poppedElement);            // Resolve the promise with the deleted post
+                const poppedElement = posts.pop();
+                resolve(poppedElement);
             } else {
-                reject("ERROR: No posts available to delete"); // If no posts to delete, reject the promise
+                reject("ERROR: No posts available to delete");
             }
-        }, 1000); // Simulate 1 second delay for deleting a post
+        }, 1000);
     });
 }
 
-// Function to handle post creation, activity update, and post deletion using async/await
 async function managePosts() {
     try {
-        // Wait for both createPost and updateLastUserActivityTime to complete using Promise.all
-        const [_, lastActivityTimeResult] = await Promise.all([
-            createPost({ title: 'POST5', body: 'This is POST5' }),  // Creating a new post
-            updateLastUserActivityTime()                            // Updating last activity time
-        ]);
-
-        // Log the posts and the updated lastActivityTime
+        const [_, lastActivityTimeResult] = await Promise.all([createPost({ title: 'POST5', body: 'This is POST5' }), updateLastUserActivityTime()]);
         console.log('Posts:', posts);
         console.log('Last Activity Time:', lastActivityTimeResult);
 
-        // Delete the last post
         const deletedPost = await deletePost();
 
-        // Log the remaining posts after deleting the last one
         console.log('Deleted Post:', deletedPost.title);
         console.log('Remaining Posts:', posts);
-    } catch (error) {
+    }
+    catch (error) {
         console.log(error);
     }
 }
 
-// Execute the async function
 managePosts();
